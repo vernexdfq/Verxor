9076934f8c1870c759615b97a4a6883991ea3f30
vernexdfq <official@vernex.com.ng>
2026-09-13 03:50:58 +0100

    feat: add legacy rental index route from Vernex

---
+++ src/rental-legacy/index-route.tsx
@@ -0,0 +1,2 @@
+import { createFileRoute, redirect } from "@tanstack/react-router";
+export const Route = createFileRoute("/_authenticated/rental/")({ beforeLoad: () => { throw redirect({ to: "/rental/calls" }); } });
