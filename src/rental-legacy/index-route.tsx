9d6d004a612bb6185c680a35498a230456f16243
vernexdfq <official@vernex.com.ng>
2026-09-16 05:14:39 +0100

    chore: remove rental-legacy index-route

--- src/rental-legacy/index-route.tsx
+++
@@ -1,2 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-export const Route = createFileRoute("/_authenticated/rental/")({ beforeLoad: () => { throw redirect({ to: "/rental/calls" }); } });
