a8b20c14ab21dbf7be2f77ca053694fe06d51438
vernexdfq <official@vernex.com.ng>
2026-09-22 12:33:40 +0100

    pages-more re-exports individual page modules

--- src/pages-more.tsx
+++ src/pages-more.tsx
@@ -1,1 +1,5 @@
-SEE_FILE
+export { HomePage } from './page-home';
+export { HistoryPage } from './page-history';
+export { FundPage } from './page-fund';
+export { NumbersPage } from './page-numbers';
+export { ProfilePage } from './page-profile';
